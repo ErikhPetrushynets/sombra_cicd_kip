aeb7e42e80e8d61f30b4133285c60a82fcee7b27
erikhpetrushynets <erikhpetrushynets@ERIKHPETRUS998B>
2023-12-04 15:53:43 +0200

    trying

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,11 @@
 module.exports = function(config) {
     config.set({
-        reporters: ['junit'],
-        junitReporter: {
-          outputDir: process.env.JUNIT_REPORT_PATH,
-          outputFile: process.env.JUNIT_REPORT_NAME,
-          useBrowserName: false
-        },
+      reporters: ['progress', 'junit'],
+      junitReporter: {
+        outputDir: './junit/',
+        outputFile: 'test-results.xml',
+        useBrowserName: 'Chrome',
+      },
       plugins: [
         'karma-junit-reporter'
       ],
